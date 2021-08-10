f49aa63eb05ff4f602b5a557fd115d013e82660c
yslle <dltmddus9082@naver.com>
2021-08-10 17:58:54 +0900

    퍼즐게임_엔딩

---
+++ game1_ending.lua
@@ -0,0 +1,72 @@
+-----------------------------------------------------------------------------------------
+--
+-- game1_ending.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+function scene:create( event )
+	local sceneGroup = self.view
+	
+	local bg = display.newImageRect("image1_1/bg_puzzle.png", 1280, 720)
+	bg.x, bg.y = display.contentWidth*0.5, display.contentHeight*0.5
+	sceneGroup:insert(bg)
+	
+	local roseImg = display.newImageRect("image1_1/rose.png", 600, 600)
+	roseImg.x, roseImg.y = display.contentWidth*0.5, display.contentHeight*0.5
+	sceneGroup:insert(roseImg)
+
+	-- 다음 장면으로 이동 --
+
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+	end	
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
