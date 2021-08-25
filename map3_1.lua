34ec2d92d1c329072600b2b2c8d00aa9675c6bb3
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-08-25 19:32:20 +0900

    mod: 대사 오류 수정, 네로가 바다 근처로 가면 대사 출력되도록 수정

--- map3_1.lua
+++ map3_1.lua
@@ -16,14 +16,14 @@ function scene:create( event )
 	bg.x, bg.y = cx, cy
 	sceneGroup:insert(bg)
 
-	local npc = display.newImageRect("image/image2/mermaid_1.png", 223, 342)
+	local npc = display.newImageRect("image/image2/mermaid_1.png", 223 * 0.9, 342 * 0.9)
 	npc.x, npc.y = 860, 450
 	sceneGroup:insert(npc)
 
 	local sail = display.newImageRect("image/image2/1-2.png", 1280, 720)
 	sail.x, sail.y = cx, cy
 
-	local bird = display.newImageRect("image/char/bird_default.png", 60, 75)
+	local bird = display.newImageRect("image/char/bird_default.png", 73, 82)
 	bird.x, bird.y = display.contentWidth * 0.22, display.contentHeight * 0.65
 	sceneGroup:insert(bird)
 
@@ -50,21 +50,6 @@ function scene:create( event )
 	sceneGroup:insert(nero);
 	sceneGroup:insert(sail)
 
-
-	-- 좌표 알아내기용 이벤트
-	local function tab( event )
-		if ( event.phase == "began" ) then
-			print("touched")
-		elseif ( event.phase == "moved" ) then
-			print(event.x .. ", " .. event.y)
-		elseif ( event.phase == "ended") then
-			print("ended")
-		end
-		return true
-	end
-
-	Runtime:addEventListener("touch", tab)
-
 	local function nextScene()
 		-- transition.fadeOut(sceneGroup, {time = 300})
 		local options = {
@@ -85,104 +70,31 @@ function scene:create( event )
 	sceneGroup:insert(text2)
 	text2.alpha = 0
 
-	-- 인어와 대화할 때 사용 (탭 이벤트가 겾쳐서 추가로 선언) --
+	-- 인어와 대화할 때 사용 (탭 이벤트가 겹쳐서 추가로 선언) --
 	local text3 = display.newImageRect("image/char/text_nero.png", 1150, 340)
 	text3.x, text3.y = display.contentWidth * 0.5, display.contentHeight * 0.75
 	sceneGroup:insert(text3)
 	text3.alpha = 0	
 
-	-- 네로 혼잣말 --
-	transition.to( text2, { delay = 2000, alpha = 0.8, effect = "fade", time = 900 } )
-
-	local txt = display.newText("푸른빛을 찾으려면 물속으로 들어가야 해. 물속에서는 숨을 쉴 수 없을텐데..", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-	txt:setFillColor(0)
-	txt.alpha = 0
-	transition.fadeIn( txt, { delay = 2000, effect = "fade", time = 900 } )
-
 	local nName = display.newText("네로", 282, 437, "fonts/SeoulNamsanB.ttf", 32)
-	sceneGroup:insert(nName)
 	nName.alpha = 0
-	transition.fadeIn( nName, { delay = 2000, effect = "fade", time = 900 } )
+	sceneGroup:insert(nName)
 
 	local nero2 = display.newImageRect("image/char/nero_default2.png", 360, 400)
 	nero2.x, nero2.y = display.contentWidth * 0.23, display.contentHeight * 0.285
-	sceneGroup:insert(nero2)
 	nero2.alpha = 0
-	transition.fadeIn( nero2, { delay = 2000, effect = "fade", time = 900 } )
+	sceneGroup:insert(nero2)
 
-	text2:toFront()
-	nName:toFront()
-
-	local function removeItems()
-		-- 틀릭하면 대화창 사라짐 --
-		transition.fadeOut( nero2, { effect = "fade", time = 700 } )
-		transition.fadeOut( text2, { effect = "fade", time = 700 } )
-		transition.fadeOut( nName, { effect = "fade", time = 700 } )
-		transition.fadeOut( txt, { effect = "fade", time = 700 } )
-
-		-- 앵무가 인어 데리고 옴 --
-		transition.to( bird, { delay = 1000, x = 700, y = 300, time = 2000 } )
-		transition.to( bird, { delay = 4000, x = 300, y = 470, time = 2000 } )
-		transition.to( npc, { delay = 4000, x = 550, time = 1800 } )
-	end
-
-	text2:addEventListener("tap", removeItems)
+	
 
 	-- 인어와 대화 --
 	local mName, npc2, text
 
-	local function textScene() 
-		-- 대화창 --
-		transition.to( npc, { alpha = 0.8, effect = "fade", time = 900 } )
-		transition.to( nero, { alpha = 0.8, effect = "fade", time = 900 } )
-		text2.alpha = 0
-		txt.alpha = 0
-		transition.to( text1, { alpha = 0.8, effect = "fade", time = 900 } )
-
-		-- 대화창 이름 --
-		mName = display.newText("언니인어", 282, 437, "fonts/SeoulNamsanB.ttf", 32)
-		sceneGroup:insert(mName)
-		mName.alpha = 0
-		transition.fadeIn( mName, { effect = "fade", time = 900 } )
-
-		npc2 = display.newImageRect("image/image2/mermaid_1.png", 450, 680)
-		npc2.x, npc2.y = display.contentWidth * 0.75, display.contentHeight * 0.53
-		sceneGroup:insert(npc2)
-		npc2.alpha = 0
-		transition.fadeIn( npc2, { effect = "fade", time = 900 } )
-
-		text1:toFront()
-		mName:toFront()
-
-		transition.fadeIn( nero2, { effect = "fade", time = 900 } )
-
-		-- 대사: 네로가 따라하는 부분 추가해야함 --
-		text = { }
-		text[1] = display.newText("너, 푸른빛인지 뭔지를 찾으려면 바다로 들어와야 한다고?", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[2] = display.newText("내가 도와줄 테니까, 너도 내 동생 찾는 것 좀 도와주지 않을래?", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[3] = display.newText("물속에서도 숨 쉴 수 있게 해주는 마법을 걸어줄게. 내가 하라는 대로 따라 해.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[4] = display.newText("순수한 물, 반짝이는 파랑, 그 아래 모든 생명의 숨.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[5] = display.newText("순수한 물, 반짝이는 파랑, 그 아래 모든 생명의 숨.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[6] = display.newText("(물거품 추가)", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[7] = display.newText("마법을 걸었으니 이제 물속에서도 숨을 쉴 수 있을 거야.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-		text[8] = display.newText(" ", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
-
-		text[1]:setFillColor(0)
-
-		for i = 2, 7 do
-			text[i].alpha = 0
-			text[i]:setFillColor(0)
-		end
-	end	
-
-	npc:addEventListener("tap", textScene)
-
+	
 	-- 탭 하면 다음 text --
 	local j = 2
 	local function nextText()
-		if j > 1 then
-			text[j - 1].alpha = 0
-		end
+		text[j - 1].alpha = 0
 
 		-- 네로가 인어 말 따라하는 장면, 타이핑 대신 일단 클릭으로.. --
 		if j == 5 then
@@ -212,6 +124,8 @@ function scene:create( event )
 			mName.alpha = 0
 			npc.alpha = 1
 			nero.alpha = 1
+			transition.fadeOut(npc, {time = 300})
+			bg:removeEventListener("tap", nextText)
 		end
 
 		if j < 8 then
@@ -220,8 +134,54 @@ function scene:create( event )
 		end
 	end
 
-	text1:addEventListener("tap", nextText)	
-	text3:addEventListener("tap", nextText)	
+	local function textScene() 
+		-- 대화창 --
+		transition.to( npc, { alpha = 0.8, effect = "fade", time = 900 } )
+		transition.to( nero, { alpha = 0.8, effect = "fade", time = 900 } )
+		text2.alpha = 0
+		transition.to( text1, { alpha = 0.8, effect = "fade", time = 900 } )
+
+		-- 대화창 이름 --
+		mName = display.newText("언니인어", 282, 437, "fonts/SeoulNamsanB.ttf", 32)
+		sceneGroup:insert(mName)
+		mName.alpha = 0
+		transition.fadeIn( mName, { effect = "fade", time = 900 } )
+
+		npc2 = display.newImageRect("image/image2/mermaid_1.png", 450, 680)
+		npc2.x, npc2.y = display.contentWidth * 0.75, display.contentHeight * 0.53
+		sceneGroup:insert(npc2)
+		npc2.alpha = 0
+		transition.fadeIn( npc2, { effect = "fade", time = 900 } )
+
+		text1:toFront()
+		mName:toFront()
+
+		transition.fadeIn( nero2, { effect = "fade", time = 900 } )
+
+		
+
+		-- 대사: 네로가 따라하는 부분 추가해야함 --
+		text = { }
+		text[1] = display.newText("너, 푸른빛인지 뭔지를 찾으려면 바다로 들어와야 한다고?", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[2] = display.newText("내가 도와줄 테니까, 너도 내 동생 찾는 것 좀 도와주지 않을래?", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[3] = display.newText("물속에서도 숨 쉴 수 있게 해주는 마법을 걸어줄게. 내가 하라는 대로 따라 해.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[4] = display.newText("순수한 물, 반짝이는 파랑, 그 아래 모든 생명의 숨.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[5] = display.newText("순수한 물, 반짝이는 파랑, 그 아래 모든 생명의 숨.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[6] = display.newText("(물거품 추가)", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[7] = display.newText("마법을 걸었으니 이제 물속에서도 숨을 쉴 수 있을 거야.", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+		text[8] = display.newText(" ", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+
+		text[1]:setFillColor(0)
+
+		for i = 2, 7 do
+			text[i].alpha = 0
+			text[i]:setFillColor(0)
+		end
+		bg:addEventListener("tap", nextText)
+		npc:removeEventListener("tap", textScene)
+	end	
+
+	npc:addEventListener("tap", textScene)
 
 	-- 방향키 입력시 움직이는 이벤트리스너
 	local function move( event )
@@ -242,14 +202,45 @@ function scene:create( event )
 				nero:play()
 				if (nero.x < 340) then
 					transition.moveBy(nero, {x = -nero.x, time = nero.x * 7})
-				else
-					transition.moveBy(nero, {x = -nero.x, y = -450, time = nero.x * 7})
+				-- else
+				-- 	transition.moveBy(nero, {x = nero.x, y = -150, time = (nero.x - 340) * 7})
 				end
 				transition.to(nero, {x = nero.x - 1000, time = 7000})
 			end
 		elseif (event.phase == "up") then
 			transition.cancel(nero) -- 이동 정지
 			nero:pause()
+			if (nero.x == 340) then
+				-- 네로 혼잣말 --
+				transition.to( text2, { alpha = 0.8, effect = "fade", time = 500 } )
+
+				local txt = display.newText("푸른빛을 찾으려면 물속으로 들어가야 해. 물속에서는 숨을 쉴 수 없을텐데..", text1.x, text1.y + 30, "fonts/SeoulNamsanB.ttf", 28)
+				txt:setFillColor(0)
+				txt.alpha = 0
+				transition.fadeIn( txt, { effect = "fade", time = 500 } )
+
+				transition.fadeIn( nName, { effect = "fade", time = 500 } )
+			
+				transition.fadeIn( nero2, {effect = "fade", time = 500 } )
+
+				text2:toFront()
+				nName:toFront()
+
+				local function removeItems()
+					-- 틀릭하면 대화창 사라짐 --
+					transition.fadeOut( nero2, { effect = "fade", time = 500 } )
+					transition.fadeOut( text2, { effect = "fade", time = 500 } )
+					transition.fadeOut( nName, { effect = "fade", time = 500 } )
+					transition.fadeOut( txt, { effect = "fade", time = 500 } )
+			
+					-- 앵무가 인어 데리고 옴 --
+					transition.to( bird, { delay = 1000, x = 700, y = 300, time = 2000 } )
+					transition.to( bird, { delay = 4000, x = 300, y = 470, time = 2000 } )
+					transition.to( npc, { delay = 4000, x = 550, time = 1800 } )
+				end
+			
+				text2:addEventListener("tap", removeItems)
+			end
 		end
 	end
 
